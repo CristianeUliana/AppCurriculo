ad6c5dfc31cfc3599b92ca66e1bad2efc4425175
Cristiane Goncalves Uliana <cristianeguliana@gmail.com>
2021-05-03 12:17:11 -0300

    implementando cards com experiência profissional

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
   Text,
   View,
   Image,
+  ScrollView,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather'
 Icon.loadFont()
@@ -15,53 +16,105 @@ import foto from './foto.png';
 const App = () => {
   return (
     <SafeAreaView style={styles.page}>
-      <View style={styles.cabecalho}>
-        <Image style={styles.foto} source={foto} />
-        <Text style={styles.nome}>CRISTIANE GONÇALVES ULIANA</Text>
-        <Text style={styles.funcao}>Desenvolvedora Mobile</Text>
-        <View style={styles.redesSociais}>
-          <Icon name="github" size={25} />
-          <Icon name="linkedin" size={25} />
-          <Icon name="facebook" size={25} />
+      <ScrollView>
+
+        <View style={styles.cabecalho}>
+          <Image style={styles.foto} source={foto} />
+          <Text style={styles.nome}>CRISTIANE GONÇALVES ULIANA</Text>
+          <Text style={styles.funcao}>Desenvolvedora Mobile</Text>
+          <View style={styles.redesSociais}>
+            <Icon name="github" size={25} />
+            <Icon name="linkedin" size={25} />
+            <Icon name="facebook" size={25} />
+          </View>
         </View>
-      </View>
-    
+        <View style={styles.card}>
+          <View>
+            <Text style={styles.cardHeaderText}>Experiência Profissional</Text>
+          </View>
+          <View>
+            <Text style={styles.cardContentText}>everis</Text>
+            <Text style={styles.cardContentText}>Desenvolvimento Mobile</Text>
+            <Text style={styles.cardContentText}> mar de 2021 - o momento</Text>
+          </View>
+        </View>
+        <View style={styles.card}>
+          <View>
+            <Text style={styles.cardHeaderText}>Experiência Profissional</Text>
+          </View>
+          <View>
+            <Text style={styles.cardContentText}>Nexxera</Text>
+            <Text style={styles.cardContentText}>Atendente de Suporte Técnico</Text>
+            <Text style={styles.cardContentText}>fev de 2019 - mai de 2019</Text>
+          </View>
+        </View>
+        <View style={styles.card}>
+          <View>
+            <Text style={styles.cardHeaderText}>Experiência Profissional</Text>
+          </View>
+          <View>
+            <Text style={styles.cardContentText}>EEB América Dutra Machado</Text>
+            <Text style={styles.cardContentText}>Professora de Protuguês - ACT</Text>
+            <Text style={styles.cardContentText}>fev de 2017 - ago de 2017</Text>
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  page:{
+  page: {
     backgroundColor: '#E7E7E7',
-    flex: 1
+    flex: 1,
   },
-  cabecalho:{
+  cabecalho: {
     alignItems: 'center',
     justifyContent: 'center',
     marginTop: 10,
   },
-  foto:{
-    width: 250,
-    height: 250,
+  foto: {
+    width: 180,
+    height: 180,
     borderRadius: 125
   },
-  nome:{
+  nome: {
     fontSize: 22 ,
     fontWeight: 'bold',
     marginTop: 10,
     marginBottom: 10
   },
-  funcao:{
+  funcao: {
     fontSize: 15,
     color: '#939393',
     marginBottom: 10,
   },
-  redesSociais:{
+  redesSociais: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     width: '40%',
     marginTop: 10
-  }
+  },
+  card: {
+    width: '80%',
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: '#939393',
+    marginTop: 20,
+    backgroundColor: 'white',
+    alignSelf: 'center'
+  },
+  cardHeaderText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    margin: 10,
+  },
+  cardContentText: {
+    marginHorizontal: 10,
+    marginBottom: 10,
+    fontSize: 16,
+    color: '#939393'
+  },
 });
 
 export default App;
